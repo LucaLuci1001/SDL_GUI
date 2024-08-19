9556a33619a026954bf3af091f00173c058013a8
LucaLuci1001 <158763078+LucaLuci1001@users.noreply.github.com>
2024-08-19 19:53:42 +0200

    implemented basic window functionality

--- headers/gui/Window.hpp
+++ headers/gui/Window.hpp
@@ -5,10 +5,30 @@
 #ifndef WINDOW_HPP
 #define WINDOW_HPP
 
+#include <thread>
+#include <SDL2/SDL.h>
 
 
 class Window {
+private:
+	std::thread thread;
 
+	bool closeWindow = false;
+
+protected:
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+
+public:
+	Window(char *title, int x, int y, int width, int height, uint32_t flags);
+	virtual ~Window();
+
+	bool update();
+	void handleEvents();
+	void run();
+
+	virtual void render();
+	virtual void deInit();
 };
 
 
--- source/gui/Window.cpp
+++ source/gui/Window.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "../../gui/Window.hpp"
+
+Window::Window(char *title, int x, int y, int width, int height, uint32_t flags)
+{
+	window = SDL_CreateWindow(title, x, y, width, height, flags);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	thread = std::thread(run);
+}
+
+Window::~Window()
+{}
+
+bool Window::update()
+{
+	handleEvents();
+
+	if(closeWindow)
+	{
+		deInit();
+		return false;
+	}
+
+	render();
+
+	return true;
+}
+
+void Window::handleEvents()
+{}
+
+void Window::run()
+{
+	while(true)
+	{
+		if(!update())
+		{
+			break;
+		}
+	}
+}
+
+void Window::render()
+{}
+
+void Window::deInit()
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+}
