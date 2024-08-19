90bebc13608975a3dee704432ca83f389e8e37c2
LucaLuci1001 <158763078+LucaLuci1001@users.noreply.github.com>
2024-08-19 19:29:43 +0200

    added Window class

---
+++ headers/gui/Window.hpp
@@ -0,0 +1,16 @@
+// 
+// Created by LucaLuci1001 on 19.08.24.
+//
+
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+
+
+class Window {
+
+};
+
+
+
+#endif //WINDOW_HPP
---
+++ source/gui/Window.cpp
@@ -0,0 +1,5 @@
+// 
+// Created by LucaLuci1001 on 19.08.24.
+//
+
+#include "Window.hpp"
