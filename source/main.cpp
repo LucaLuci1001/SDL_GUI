01c3e6156e150c6c09c26fe47b3dd05e54d63336
LucaLuci1001 <158763078+LucaLuci1001@users.noreply.github.com>
2024-08-20 16:55:27 +0200

    added a little example

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <SDL.h>
 
-int main()
-{
-    return 0;
+#include "../headers/gui/Window.hpp"
+
+int main() {
+	SDL_Init(SDL_INIT_VIDEO);
+
+	WindowHandler windowHandler;
+	Window window{windowHandler, "window1"};
+	Window window1{windowHandler, "window2"};
+
+	while(windowHandler.tick())
+	{}
+
+	SDL_Quit();
+
+	return 0;
 }
