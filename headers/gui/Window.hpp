d7362dc5ee92f19ec5394729febb5235f5d4328f
LucaLuci1001 <158763078+LucaLuci1001@users.noreply.github.com>
2024-08-22 15:17:55 +0200

    added handleLogic() method, can be kept running if the window is in background

--- headers/gui/Window.hpp
+++ headers/gui/Window.hpp
@@ -11,26 +11,29 @@
 
 class WindowHandler;
 
-class Window {
+class Window
+{
 private:
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 
 	bool closeWindow = false;
+	bool keepRunningInBG;
 
 public:
 	const uint32_t id;
 
-	Window(WindowHandler &windowHandler, const char *title, int x, int y, int width, int height, uint32_t flags);
-	~Window();
+	Window(WindowHandler &windowHandler, bool keepRunningInBG, const char *title, int x, int y, int width
+	               , int height, uint32_t flags);
+	virtual ~Window();
 
 	bool tick();
 	void handleEvent(SDL_Event &event);
 	virtual void handleEventCustom(SDL_Event &event);
+	virtual void handleLogic();
 	virtual void render();
 	virtual void deInit();
 };
-
 
 
 #endif //WINDOW_HPP
--- source/gui/Window.cpp
+++ source/gui/Window.cpp
@@ -6,9 +6,11 @@
 
 #include <iostream>
 
-Window::Window(WindowHandler &windowHandler, const char *title, int x, int y, int width, int height, uint32_t flags)
+Window::Window(WindowHandler &windowHandler, bool keepRunningInBG, const char *title, int x, int y, int width
+               , int height, uint32_t flags)
 	: window(SDL_CreateWindow(title, x, y, width, height, flags))
 	, renderer(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED))
+	, keepRunningInBG(keepRunningInBG)
 	, id(SDL_GetWindowID(window))
 {
 	windowHandler.addWindow(this);
@@ -41,6 +43,9 @@ void Window::handleEvent(SDL_Event &event)
 void Window::handleEventCustom(SDL_Event &event)
 {}
 
+void Window::handleLogic()
+{}
+
 bool Window::tick()
 {
 	if (closeWindow)
@@ -51,8 +56,12 @@ bool Window::tick()
 
 	if (SDL_GetWindowFlags(window) & SDL_WINDOW_SHOWN)
 	{
+		handleLogic();
 		render();
 		SDL_RenderPresent(renderer);
+	} else if (keepRunningInBG)
+	{
+		handleLogic();
 	}
 
 	return true;
